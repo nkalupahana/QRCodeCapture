22b5c306c18a5c0a7a318ed2dd9c6bba8eaf7573
nkalupahana <avishkank@gmail.com>
2024-12-14 01:04:10 -0800

    Open on boot

---
+++ app/src/main/java/la/nisa/qrcodecapture/BootReceiver.kt
@@ -0,0 +1,13 @@
+package la.nisa.qrcodecapture
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val i = Intent(context, MainActivity::class.java)
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        context.startActivity(i)
+    }
+}
